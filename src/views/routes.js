de4a5a35ef56834e29fa917445a2c27f7c066521
UlricGan <hongxiang0310@gmail.com>
2015-07-25 23:31:00 +0800

    加入listing

--- src/views/routes.js
+++ src/views/routes.js
@@ -3,11 +3,12 @@ import { Route } from 'react-router'
 
 import App from './App'
 import Recommend from './Recommend'
+import Listing from './Listing'
 
 export default (
 	<Route component={App}>
 		{/*<Route path="/" component={LoanApply} />*/}
 		<Route path="/recommend/:recommendType" component={Recommend} />
-		{/*<Route path="/list/:listType" component={} />*/}
+		<Route path="/listing/:listType" component={Listing} />
 	</Route>
 )

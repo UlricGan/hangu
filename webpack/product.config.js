86b7223e2f66c7b6a6ec1f93aebdb8f6f6332789
UlricGan <hongxiang0310@gmail.com>
2015-07-27 22:39:16 +0800

    universal css

--- webpack/product.config.js
+++ webpack/product.config.js
@@ -1,10 +1,13 @@
 var path = require('path')
 var webpack = require('webpack')
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var CleanPlugin = require('clean-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var strip = require('strip-loader');
 
 var writeStats = require('./utils/writeStats')
-var assetsPath = path.join(__dirname, '../static/dist');
+
+var relativeAssetsPath = '../static/dist'
+var assetsPath = path.join(__dirname, '../static/dist')
 
 module.exports = {
 	devtool: 'source-map',
@@ -22,7 +25,8 @@ module.exports = {
 		loaders: [
 			{ test: /\.(jpe?g|png|gif|svg)$/, loader: 'file' },
 			{ test: /\.jsx?$/, exclude: /node_modules/, loaders: [strip.loader('debug'), 'babel?stage=0&optional=runtime&plugins=typecheck']},
-			{ test: /\.scss$/, loader: ExtractTextPlugin.extract('style', 'css!autoprefixer?browsers=last 2 version!sass') }
+			{ test: /\.json$/, loader: 'json-loader' },
+			{ test: /\.styl$/, loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=2!autoprefixer?browsers=last 2 version!stylus?sourceMap=true') }
 		]
 	},
 	resolve: {
@@ -30,8 +34,10 @@ module.exports = {
 	},
 	plugins: [
 
+    new CleanPlugin([relativeAssetsPath]),
+
 		// css 文件单独拉出来
-		new ExtractTextPlugin('[name]-[chunkhash].css'),
+		new ExtractTextPlugin('[name]-[chunkhash].css', {allChunks: true}),
 
 		// ignore dev config
 		new webpack.IgnorePlugin(/\.\/dev/, /\/config$/),
@@ -60,7 +66,9 @@ module.exports = {
 		}),
 
 		function () {
-			this.plugin('done', writeStats)
+			this.plugin('done', function(stats) {
+        writeStats.call(this, stats, 'prod')
+			})
 		}
 
 	]
